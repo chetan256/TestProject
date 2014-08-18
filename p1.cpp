4363a86558301327adbbd5d1d538d515e150c263
chetan <chetan.c256@gmail.com>
2014-08-18 14:35:45 +0530

    Create p1.cpp

---
+++ p1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int div(int,int);
+int main()
+{
+    cout<<div(2,1);
+    cout<<div(3,2);
+    cout<<div(4,4);
+    cout<<div(8,2);
+}
+int div(int a,int b)
+{
+    return a/b;
+}
