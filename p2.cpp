dfb1466cc28b7017596b1bf54bd11d13392635d6
chetan <chetan.c256@gmail.com>
2014-08-19 12:46:48 +0530

    Update p2.cpp

--- p2.cpp
+++ p2.cpp
@@ -1,11 +1,28 @@
 #include<iostream>
+#include<stdio.h>
 #include "p1.cpp"
 using namespace std;
 int main()
 {
-  cout<<div(2,1);
-  cout<<div(3,2);
-  cout<<div(4,4);
-  cout<<div(8,2);
+  FILE *f;
+  int a,b,val,i;
+  f=fopen("testresults.txt","r");
+  if(f==NULL)
+  {
+      cout<<"error\n";
+  }
+  else{
+  i=0;
+  while(!feof(f))
+  {
+      fscanf(f,"%d %d %d",&a,&b,&val);
+      if(div(a,b)==val)
+        cout<<"Test Case"<<i<<" Passed\n";
+      else
+        cout<<"Test Case"<<i<<" Failed\n";
+    i++;
+  }
+}
   return 0;
   }
+
